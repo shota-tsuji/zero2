74b67b60e923cffda68cba15a0773d65480542b5
shota-tsuji <shotatuzi@gmail.com>
2022-02-06 01:36:28 +0900

    feat: Postgres connection is tested at localhost.

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::net::TcpListener;
+use zero2::configuration::get_configuration;
+use zero2::startup::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
+}
