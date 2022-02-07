0e4919061ed6be0f70a920fe01815b9d9c9ad29b
shota-tsuji <shotatuzi@gmail.com>
2022-02-08 06:24:27 +0900

    feat: Request is traced by logger.

--- src/startup.rs
+++ src/startup.rs
@@ -3,6 +3,7 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use actix_web::middleware::Logger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the connection pool in a smart pointer
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone()) // Register the connection as part of the application state
