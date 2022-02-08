91e5ec9f19468053712c678ac229ba9a5751818a
shota-tsuji <shotatuzi@gmail.com>
2022-02-09 02:44:52 +0900

    feat: Consistent request-id is used on a whole request. / Secret is used explicitly.

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,19 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
+
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the connection pool in a smart pointer
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone()) // Register the connection as part of the application state
